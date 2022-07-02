39b77a63fae9653c37757b7b3416ed39490e6a53
mo7amedehab97 <61945138+mo7amedehab97@users.noreply.github.com>
2022-07-02 09:11:19 +0300

    Time: 90 ms (70.50%), Space: 41.5 MB (94.57%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high){
+let count = Math.floor((high - low) / 2)
+if(low % 2 !== 0 || high % 2 !== 0) count++
+return count
+}
